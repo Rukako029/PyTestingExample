9d100a209c360257fc95f2eaf811c95c03cf7e0b
HiteshDubey <reenadubey7000@gmail.com>
2025-03-12 12:39:21 +0530

    fixed bugs  in your main function file

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 def add(a, b):
     return a + b
 
-
-def subtract(a, b):
-    return a + b
+## corrected with substract sign
+def substract(a, b):
+    return a - b
 
 
 def multiply(a, b):
@@ -11,4 +11,8 @@ def multiply(a, b):
 
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9 / 5)
+    return multiply(substract(fahrenheit, 32), 5 / 9) ## corrected  the with right formula
+
+
+
+print(convert_fahrenheit_to_celsius(273))
